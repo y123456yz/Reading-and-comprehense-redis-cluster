4535aea64ea3133fc42a78e0c249e6c4035253a8
yazhou.yang <yazhou.yang@husor.com>
2017-02-04 18:46:21 +0800

    redis-benchmark support mset,mget,multi-del

--- redis-3.0/src/release.h
+++ redis-3.0/src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "53fcd65b"
-#define REDIS_GIT_DIRTY "241"
-#define REDIS_BUILD_ID "centlhw1-1485168045"
+#define REDIS_GIT_SHA1 "9d9d4644"
+#define REDIS_GIT_DIRTY "0"
+#define REDIS_BUILD_ID "centlhw1-1486205165"
